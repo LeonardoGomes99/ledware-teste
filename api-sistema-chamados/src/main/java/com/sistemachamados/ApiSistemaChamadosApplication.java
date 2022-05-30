eaa16e0c922cf908aaac298067a4f70d611a9fe4
Leonardo Gomes Da Silva <35742346+LeonardoGomes99@users.noreply.github.com>
2022-05-30 20:14:24 -0300

    Subindo API

---
+++ api-sistema-chamados/src/main/java/com/sistemachamados/ApiSistemaChamadosApplication.java
@@ -0,0 +1,13 @@
+package com.sistemachamados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSistemaChamadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSistemaChamadosApplication.class, args);
+	}
+
+}
